9182fcb25bc839f466928a8cb659bbce30e9210e
Daniel Dominguez <danidomin19@gmail.com>
2023-04-26 17:35:06 +0200

    try

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,26 +12,15 @@ interface FileMetadata {
   slug: string;
 }
 
-async function getSitemapItems (): Promise<Array<any>> {
+async function getSitemapItems () {
   const allPosts = await getAllFilesMetadata()
   return allPosts.map((post: FileMetadata) => ({
     url: `https://gatomundo.com/blog/${post.categories[0]}/${post.slug}`,
-    lastModified: new Date(post.date)
+    lastModified: new Date()
+    // lastModified: new Date(post.date)
   }))
 }
 
-export default async function sitemap (): Promise<MetadataRoute.Sitemap> {
-  const items = await getSitemapItems()
-
-  return [
-    {
-      url: 'https://gatomundo.com',
-      lastModified: new Date()
-    },
-    {
-      url: 'https://gatomundo.com/about',
-      lastModified: new Date()
-    },
-    ...items
-  ]
+export default async function sitemap (): Promise<{ url: string; lastModified?: string | Date | undefined; }[]> {
+  return await getSitemapItems()
 }
