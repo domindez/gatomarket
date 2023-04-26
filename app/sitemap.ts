d4abead2686ac27ba3cb3f4b0e2d939a116e8300
Daniel Dominguez <danidomin19@gmail.com>
2023-04-26 17:54:06 +0200

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,29 +1,6 @@
 import { MetadataRoute } from 'next'
-import { getAllFilesMetadata } from '../lib/mdx'
-
-interface FileMetadata {
-  title: string;
-  extract: string;
-  author: string;
-  img: string;
-  categories: Array<string>;
-  keywords: Array<string>;
-  date: string;
-  slug: string;
-}
-
-function getSitemapItems () {
-  const allPosts = getAllFilesMetadata()
-  return allPosts.map((post: FileMetadata) => ({
-    url: `https://gatomundo.com/blog/${post.categories[0]}/${post.slug}`,
-    lastModified: new Date()
-    // lastModified: new Date(post.date)
-  }))
-}
 
 export default function sitemap (): MetadataRoute.Sitemap {
-  const items = getSitemapItems()
-
   return [
     {
       url: 'https://acme.com',
@@ -36,7 +13,6 @@ export default function sitemap (): MetadataRoute.Sitemap {
     {
       url: 'https://acme.com/blog',
       lastModified: new Date()
-    },
-    ...items
+    }
   ]
 }
