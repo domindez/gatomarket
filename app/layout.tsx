a7a90558641cf1a6543106eea0de200a5e9768f8
Daniel Dominguez <danidomin19@gmail.com>
2023-04-29 11:23:35 +0200

    tgm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/inline-script-id */
 import React from 'react'
 import '../sass/globals.scss'
 import Header from '@/components/Header'
@@ -19,6 +20,15 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <Script
         src='https://www.googletagmanager.com/ns.html?id=GTM-KDFWVBD'
         strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-KDFWVBD');
+          `
+        }}
       />
       <html className={sairaCondensed.className}>
         <body>
