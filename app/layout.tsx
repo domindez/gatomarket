5a99c02ee9a8b83a62651c5585ca7950a7c78327
Daniel Dominguez <danidomin19@gmail.com>
2023-04-29 11:31:43 +0200

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/inline-script-id */
 import React from 'react'
 import '../sass/globals.scss'
 import Header from '@/components/Header'
@@ -18,15 +17,15 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Script
-        src='https://www.googletagmanager.com/ns.html?id=GTM-KDFWVBD'
+        id='gtag-base'
         strategy='afterInteractive'
         dangerouslySetInnerHTML={{
           __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', 'GTM-KDFWVBD');
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KDFWVBD');
           `
         }}
       />
